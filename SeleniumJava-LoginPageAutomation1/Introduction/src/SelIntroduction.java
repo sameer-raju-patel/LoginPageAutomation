8a452c97d74bebd1af9e3218a217c38843b9ef8d
sameer-raju-patel <148366874+sameer-raju-patel@users.noreply.github.com>
2023-10-19 23:21:35 -0400

    Update SelIntroduction.java

--- SeleniumJava-LoginPageAutomation1/Introduction/src/SelIntroduction.java
+++ SeleniumJava-LoginPageAutomation1/Introduction/src/SelIntroduction.java
@@ -9,34 +9,55 @@ public class SelIntroduction {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		
+		String name = "Sameer Patel";
+		String emailID = "srpatel19126@gmail.com";
 		System.setProperty("Webdriver.chrome.driver", "/users/sam/Documents/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String password = getPassword(driver);
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
-		driver.findElement(By.id("inputUsername")).sendKeys("Sameer");
-		driver.findElement(By.name("inputPassword")).sendKeys("Sam@78699");
+		driver.findElement(By.id("inputUsername")).sendKeys(name);
+		driver.findElement(By.name("inputPassword")).sendKeys("Sam@776482");
 		driver.findElement(By.className("signInBtn")).click();
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		driver.findElement(By.linkText("Forgot your password?")).click();
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//input[@placeholder = 'Name']")).sendKeys("Sameer Patel");
-		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("srpatel191296@gmail.com");
+		driver.findElement(By.xpath("//input[@placeholder = 'Name']")).sendKeys(name);
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys(emailID);
 		driver.findElement(By.cssSelector("input[placeholder='Email']")).clear();
-		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("srpatel191296@gmail.com"); 
+		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys(emailID); 
 		driver.findElement(By.xpath("//input[@type='text'][3]")).sendKeys("8689866690"); 
 		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
 		System.out.println (driver.findElement(By.className("infoMsg")).getText());
 		driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
 		Thread.sleep(1000);
-		driver.findElement(By.cssSelector("input#inputUsername")).sendKeys("Sameer Patel");
-		driver.findElement(By.cssSelector("input[type*=pass]")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("input#inputUsername")).sendKeys(name);
+		driver.findElement(By.cssSelector("input[type*=pass]")).sendKeys(password);
 		driver.findElement(By.id("chkboxOne")).click();
 		driver.findElement(By.name("chkboxTwo")).click();
 		driver.findElement(By.className("submit")).click();
 		Thread.sleep(1000);
 		System.out.println(driver.findElement(By.tagName("p")).getText());
 		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+		System.out.println(driver.findElement(By.cssSelector("h2")).getText());
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello "+name+",");
+		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		driver.close();
+	}
+	
+	public static String getPassword(WebDriver driver) throws InterruptedException
+	{
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String PasswordText= driver.findElement(By.className("infoMsg")).getText();
+		String[] StringArray1 = PasswordText.split("'");
+		String[] StringArray2 = StringArray1[1].split("'");
+		String password = StringArray2[0]; 
+		return password;
 		
 	}
 
